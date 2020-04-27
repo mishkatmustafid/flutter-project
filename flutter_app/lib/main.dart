8103250f47cbb2357fd54abcaa3ca027dcf39bb4
mishkatmustafid <mishkatmustafid@gmail.com>
2020-04-27 11:26:26 +0600

    Moved the whole tree to a stateless widget for instantanous changes on the mobile screen

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,27 +1,37 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text('My First App'),
-      centerTitle: true,
-      backgroundColor: Colors.red[600],
-    ),
-    body: Center(
-      child: Text(
+  home: Home()
+));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My First App'),
+        centerTitle: true,
+        backgroundColor: Colors.red[600],
+      ),
+      body: Center(
+        child: Text(
           'Hello People.',
           style: TextStyle(
-              fontSize: 25.0,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 2.0,
-              color: Colors.grey[600],
+            fontSize: 25.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.grey[600],
+            fontFamily: 'IndieFlower',
           ),
+        ),
       ),
-    ),
-    floatingActionButton:FloatingActionButton(
-      onPressed: (){},
-      child: Text('Click!'),
-      backgroundColor: Colors.red[600],
-    ) ,
-  ),
-));
+      floatingActionButton:FloatingActionButton(
+        onPressed: (){},
+        child: Text('Click!'),
+        backgroundColor: Colors.red[600],
+      ) ,
+    );
+  }
+}
+
+
